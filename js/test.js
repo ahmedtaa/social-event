1448b63c2eec2902a5ca0fbb453ae6ee574f2f38
ahmed tawfik ali <kalkolty@hotmail.com>
2021-12-21 00:17:25 +0200

    index.html Mobile view Done

---
+++ js/test.js
@@ -0,0 +1,33 @@
+var speakersData = {
+  name: 'Ahmed',
+  speaker_image: './img/profile-pic.jpg',
+  about: 'some text random form some text random formsome text random form',
+  more: 'some text random form some text random formsome text random form',
+};
+var speakers = document.querySelector('.speaker-cards');
+load(2);
+speakers.parentNode.innerHTML =
+  speakers.parentNode.innerHTML +
+  ` <button class="more-btn" onclick="loadMore()">MORE <i class="arrow down"></i> </button>  `;
+function loadMore() {
+  load(4);
+  document.querySelector('.more-btn').remove();
+}
+function load(x) {
+  var speakers = document.querySelector('.speaker-cards');
+  for (let i = 0; i < x; i++) {
+    speakers.innerHTML =
+      speakers.innerHTML +
+      `
+    <li class="card">
+        <img src="${speakersData.speaker_image}" alt="">
+        <div class="speaker-info">
+            <h2 class="name">${speakersData.name}</h2>
+            <p class="about">${speakersData.about}</p>
+            <hr class="line-gray">
+            <p class="more">${speakersData.more}</p>
+        </div>
+    </li>
+    `;
+  }
+}
